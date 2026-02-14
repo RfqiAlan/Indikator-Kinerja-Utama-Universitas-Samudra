86b1ecb7fea871fd90ebfe745992c2dc24e0ab0a
RfqiAlan <rifqialan46475@gmail.com>
2026-02-15 00:00:19 +0800

    feat:new logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-blue-600" />
+                    <x-application-logo class="w-48 h-48" />
                 </a>
             </div>
 
