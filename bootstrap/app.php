9778dc42d142e441b1534ceb63b01b5dd6efa191
RfqiAlan <rifqialan46475@gmail.com>
2026-04-24 01:21:36 +0800

    fix: register missing 'drive.connected' middleware alias in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'role'            => \App\Http\Middleware\RoleMiddleware::class,
+            'drive.connected' => \App\Http\Middleware\EnsureGoogleDriveConnected::class,
         ]);
         $middleware->trustProxies(at: '*');
 
